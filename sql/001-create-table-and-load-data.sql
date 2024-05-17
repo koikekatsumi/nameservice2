d906a554b390496027080119005454aafde3c7ad
koikekatsumi <katsumikoike724@gmail.com>
2024-05-18 07:45:30 +0900

    DB周りの環境構築

---
+++ sql/001-create-table-and-load-data.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS names;
+CREATE TABLE names (
+ id int unsigned AUTO_INCREMENT,
+ name VARCHAR(20) NOT NULL,
+ PRIMARY KEY(id)
+);
+INSERT INTO names (name) VALUES ('catherine');
+INSERT INTO names (name) VALUES ('Jonathan');
