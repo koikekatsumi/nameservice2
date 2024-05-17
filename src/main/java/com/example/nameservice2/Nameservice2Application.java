ba4f4dc9d19c40e8f441dbc39eafd128dbf72363
koikekatsumi <katsumikoike724@gmail.com>
2024-05-18 02:25:38 +0900

    プロジェクトの初期設定

---
+++ src/main/java/com/example/nameservice2/Nameservice2Application.java
@@ -0,0 +1,13 @@
+package com.example.nameservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nameservice2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nameservice2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nameservice2/Nameservice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nameservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nameservice2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
