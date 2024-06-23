4de707bd6a63c69e9a33ef19eb8fc3b071de0990
koikekatsumi <katsumikoike724@gmail.com>
2024-06-23 17:47:00 +0900

    ワイルドカードインポートは回避とPRの概要の＃の後の半角スペースを設定

--- src/main/java/com/example/nameservice2/CoustomeExceptionHandler.java
+++ src/main/java/com/example/nameservice2/CoustomeExceptionHandler.java
@@ -4,7 +4,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.ZonedDateTime;
