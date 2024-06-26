82f043c78ea087244b802c65dca2a4052d6542c8
koikekatsumi <katsumikoike724@gmail.com>
2024-06-26 14:28:09 +0900

    ワイルドカード回避の再修正

--- src/main/java/com/example/nameservice2/NameController.java
+++ src/main/java/com/example/nameservice2/NameController.java
@@ -1,10 +1,11 @@
 package com.example.nameservice2;
 
-
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
 
 @RestController
 public class NameController {
